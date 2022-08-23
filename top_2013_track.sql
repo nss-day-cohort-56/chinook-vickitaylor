044605e5ad1213aad788ccf83318040c539ffb70
Vicki Taylor <vtaylor85@live.com>
2022-08-23 14:18:43 -0500

    top track in 2013

---
+++ top_2013_track.sql
@@ -0,0 +1,19 @@
+-- Provide a query that shows the most purchased track(s) of 2013.
+
+WITH TopTrack AS (
+    SELECT 
+        t.Name AS TrackName,
+        COUNT(il.InvoiceLineId) AS Sold
+    FROM InvoiceLine il
+    JOIN Invoice i ON i.InvoiceId = il.InvoiceId
+    JOIN Track t ON t.TrackId = il.TrackId
+    WHERE i.InvoiceDate BETWEEN  "2013-01-01" AND "2013-12-31"
+    GROUP BY t.Name
+)
+SELECT 
+    TrackName
+FROM TopTrack
+WHERE Sold = (
+    SELECT MAX(Sold)
+    FROM TopTrack
+);
